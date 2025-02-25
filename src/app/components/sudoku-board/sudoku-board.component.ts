333867e83ac19e1021fb0229948e2598d6031670
csalaz5 <csalaz5@bancodebogota.com.co>
2025-02-25 12:48:04 -0500

    feat (keydown): Se añade funcionalidades con teclado
    Se añade el backspace para eliminar, y las flechas para moverse por el tablero

--- src/app/components/sudoku-board/sudoku-board.component.ts
+++ src/app/components/sudoku-board/sudoku-board.component.ts
@@ -14,6 +14,10 @@ import { Sudoku, SudokuField } from "../../core/models/sudoku.model"; // Adjust 
 import { CommonModule } from "@angular/common";
 import { SudokuGridComponent } from "../sudoku-grid/sudoku-grid.component";
 
+const between = (newValue: number, min: number, max: number) => {
+  return Math.min(Math.max(newValue, min), max);
+};
+
 @Component({
   selector: "app-sudoku-board",
   imports: [CommonModule, SudokuGridComponent],
@@ -38,8 +42,33 @@ export class SudokuBoardComponent implements OnChanges {
     console.log("changes: ", changes["sudoku"]);
   }
 
+  @HostListener("window:keydown.backspace")
+  onBackspace(event: KeyboardEvent) {
+    this.erase();
+  }
+
+  @HostListener("window:keydown.arrowUp")
+  onArrowUp(event: KeyboardEvent) {
+    this.moveFocus(-1,0);
+  }
+
+  @HostListener("window:keydown.arrowDown")
+  onArrowDown(event: KeyboardEvent) {
+    this.moveFocus(1,0);
+  }
+
+  @HostListener("window:keydown.arrowLeft")
+  onArrowLeft(event: KeyboardEvent) {
+    this.moveFocus(0,-1);
+  }
+
+  @HostListener("window:keydown.arrowRight")
+  onArrowRight(event: KeyboardEvent) {
+    this.moveFocus(0,1);
+  }
+
   @HostListener("window:keydown", ["$event"])
-  onKeyDown(event: KeyboardEvent) {
+  onKeyDown(event: KeyboardEvent) {  
     const number = parseInt(event.key, 10);
     if (!this.activeField || isNaN(number) || number < 1 || number > 9) {
       return;
@@ -51,12 +80,33 @@ export class SudokuBoardComponent implements OnChanges {
   insertNumber(number: number) {
     console.log("activeField: ", this.activeField());
     console.log("insertando el: ", number);
-    let field = this.activeField();
-    if (field && !field?.readonly) {
-      field.value = number;
-      console.log("field",field);
+    if (this.activeField() && !this.activeField().readonly) {
+      this.activeField().value = number;
       console.log("this.activeField",this.activeField());
     }
+  }
+
+  erase(){
+    if (this.activeField() && !this.activeField().readonly) {
+      this.activeField().value = undefined;
+      this.activeField().notes = [];         
+    }
+  }
+
+  currentRow(): number {
+    console.log("currentRow",this.sudoku().findIndex(row => row.indexOf(this.activeField()) !== -1));
+    return this.sudoku().findIndex(row => row.indexOf(this.activeField()) !== -1);
+  }
+
+  currentCol(): number {
+    console.log("CurrentCol", this.sudoku()[this.currentRow()].indexOf(this.activeField()));
+    return this.sudoku()[this.currentRow()].indexOf(this.activeField());
+  }
+
+  moveFocus(row: number, col: number){
+    let newRow = between (this.currentRow() + row, 0, 8);
+    let newCol = between (this.currentCol() + col, 0, 8);
+    this.activeField.set(this.sudoku()[newRow][newCol]);
   }
 
   checkFinished(){
